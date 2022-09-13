a20aff34ae0b350d0334779869d1b4d64e700dd2
kingHorizon <truthmf@gmail.com>
2022-09-13 19:56:51 -0400

    Update tf_analysis_bazinga_shortened.py
    reformatted for readability

--- simpleTraderAug2022vetting/tf_analysis_bazinga_shortened.py
+++ simpleTraderAug2022vetting/tf_analysis_bazinga_shortened.py
@@ -1,3 +1,4 @@
+### import modules and libraries ###
 #%pip install requests_cache
 #%pip install pandas==1.3.5
 import yfinance as yf
@@ -7,11 +8,37 @@ import numpy as np
 import finta as fta
 import itertools
 from scipy.signal import argrelextrema
-#import requests_cache
-def main(sym='SPY',interval="1d",period='max',start=None,end=None,candle_range=None):
-    # possible periods: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max
-    # possible intervals: 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
-    # sanitize paramters
+
+
+def get_historical_trading_signals_for_symbol(sym='SPY',interval="1d",period='max',start=None,end=None,candle_range=None,signals=['hammer']):
+    
+    """ 
+        DESCRIPTION :
+        A short and sweet function that populates a pandas-dataframe (a table)
+        with a user's preffered historical trade signals of a single equity-symbol.
+        
+        PARAMATER OPTIONS :
+        - periods: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max
+        - intervals: 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
+        - candle_range : any integer
+        - start : start date
+        - end : end date
+        - sym : symbol/ticker/equity to collect signals for 
+        - signals : a list of strings representing a signal to be chosen from any of the following
+            -- "hammer" : 
+            -- "sentiment" : provides a value between -1 and 1 ranking the sentiment of news from over a time period 
+                --- NOTE : not capable of collecting historical data yet. can only assign signal to the immediate day
+        (these options are pulled from the yfinance python module. https://aroussi.com/post/python-yahoo-finance
+        "m" is short hand for minute "d" is short for day, "mo" is month, "y" year, "ytd" year-to-date, 
+        "max" maximum period for intraday intervals/timeframes/candle-lengths... which is approximately 30 days)
+        
+    """
+    
+    
+    ##########################################
+    #### initialize data and sanitize paramters
+    ##########################################
+    
     if period =='max' and (('m' in interval and 'o' not in interval) or ('h' in interval)):
         if interval in ['2m','5m','10m','15m']:
             period = '1mo'
@@ -24,110 +51,137 @@ def main(sym='SPY',interval="1d",period='max',start=None,end=None,candle_range=N
             start= (dtnow - datetime.timedelta(days=55)).strftime('%Y-%m-%d')
             period=None
             print('new interval',start,end)
+            
     #get price data for symbol
     ticker = yf.Ticker(sym)
     df = ticker.history(period=period,interval = interval,start=start,end=end)
     time.sleep(1)
-    #print(type(df))
-    #print(df.columns) # NOTE first letter of column names are capitlaized 
-    #print(f'\033[1;32;40m WARNING !!!!! first letter of column names are capitlaized \033[0;37;40m ')
+    
+    # rename the dataframe/table columns so that they are easier to work with 
     df = df.rename(columns={'Open':'open','Close':'close','High':'high','Low':'low','Volume':'volume'})
+    # create a datetime object column for easier temporal manipulations
     df['datetime']=dts=list(pd.to_datetime(df.index))
+    # convert datetime objects from datetime column to timezone aware datetime objects for easier temporal manipulations
     dtn = dts[0]
     if dtn.tzinfo is None or dtn.tzinfo.utcoffset(dtn) is None:
         print('naive datetimes converted to utc')
         df['datetime']=list(pd.to_datetime(df.index,utc=True))
+        
+    # make sure we have the desired number of candles
     tot_len = len(df['datetime'])
     if candle_range==None:
         candle_range = tot_len
-    if tot_len-1-candle_range < 0:
-        raise ValueError('not enough candles available online')
-    df = df.loc[df.index[tot_len-1-candle_range]:,:]
     if len(df['datetime']) < candle_range:
         raise ValueError('not enough candles available online')
-    #print('not anymore')
+
+    df_d = {} # this variable will be used to keep non scalar analysis data about indicators and time series
+    
+    
+    ##########################################
+    ##########################################
+    #### define historical signal generator functions
+    ##########################################
+    ##########################################
+    
+    
+    ### make short hand for accessing dataframe/tablel columns
+    cls, ops, lws, hhs, vls = df['close'],df['open'],df['low'],df['high'],df['volume']
+    
+    
+    #############
+    ### - "rsi" : relative strngth index - technical analysis
+        def rsi(df,rsi_len=21):
+            df['rsi'] = fta.TA.RSI(df,rsi_len)
+            # END FUNCTION
+        rsi(df,rsi_len=7)
+        
+    ############# 
+    ### -- "hammer" - candle pattern signal
+    if 'hammer' in signals:
+        def hammer(df,cls,ops,hhs,lws,wick_to_body_ratio=2,wick_to_wick_ratio=1.5):
+            hmrs=[]
+            uphs,dwnhs=[],[]
+            for i,h0 in enumerate(hhs):
+                bod=cls[i]-ops[i]
+                l0,cl0,op0=lws[i],cls[i],ops[i]
+                try:
+                    if bod<0: # down candle
+
+                        uph = abs(l0-cl0)/abs(h0-op0) # up/bull signal
+                        dwnh = abs(h0-op0)/abs(l0-cl0)
+                    elif bod>0: # up candle
+                        uph = abs(l0-op0)/abs(h0-cl0)
+                        dwnh = abs(h0-cl0)/abs(l0-op0)
+                    else:
+                        hmrs.append(0)
+                        continue
+                    if uph > wick_to_wick_ratio :
+                        wick_size= (min(ops[i],cls[i])-l0) #lower :NOTE should be positive
+                        if abs(wick_size) / abs(bod) >  wick_to_body_ratio:
+                            expected_percent_change = (wick_size+abs(bod))/cl0
+                            hmrs.append(expected_percent_change)
+                        else:
+                            hmrs.append(0)
+                    elif dwnh > wick_to_wick_ratio:
+                        wick_size= (max(ops[i],cls[i])-h0) # upper # NOTE should be negative
+                        if abs(wick_size) / abs(bod) >  wick_to_body_ratio:
+                            expected_percent_change = (wick_size-abs(bod))/cl0
+                            hmrs.append(expected_percent_change)
+                        else:
+                            hmrs.append(0)
+                    else:
+                        hmrs.append(0)
+                except:
+                    hmrs.append(0)
+            df['hammers'] = hmrs
+            #print(hmrs,'hammers')
+            # END FUNCTION
+        hammer(df,cls,ops,hhs,lws,wick_to_body_ratio=2,wick_to_wick_ratio=1.5)
+        
+    ############# 
+    ### -- "sentiment" - qualitative analysis (news based)
+    if "sentiment" in signals :
+        def sentiment(df,df_d):
+            #https://medium.datadriveninvestor.com/sentiment-analysis-of-stocks-from-financial-news-using-python-82ebdcefb638?gi=a8849e546f46
+            df['sentiments'] = [0]*len(df['hammers'])
+            print('WARNING - Sentiments analysis not yet implemented')
+            pass
+            # END FUNCTION
+        sentiment(df,df_d)
+
+    ##########################################
+    #### Print function and signal information to user device (i.e. terminal)
+    ##########################################
     #print(df.columns)
-    df_d = {} # to house non singleton analysis data about indicators and time series
-    
+    print(f'\033[1;34;40m symbol: {sym} timeframe: {interval}\033[0;37;40m')
+    print('hammer signals:\n',df['hammers'][-10:])
+    print('current sentiment score (timeframe unknown... max data pulled):\n', df['sentiments'][-1:])
+    print(f'\033[1;33;40m {sym} timeframe {interval} current trade reccomendation:\n', df['hammers'][-1],'\n\033[0;37;40m')
+    # NOTE this function should send data up to a ranking function that takes inputs for how to weight each indicator signal and aggregate them into a tradeable prediction about price movement 
+    return df,df_d
     
     
     ###
     ###
     ###
-    
-    
-    
-    #get signals
-    cls,ops,lws,hhs,vls = df['close'],df['open'],df['low'],df['high'],df['volume']
-    # - rsi
-    def rsi(df,rsi_len=21):
-        df['rsi'] = fta.TA.RSI(df,rsi_len)
-    # -- hammer
-    def hammer(df,cls,ops,hhs,lws,wick_to_body_ratio=2,wick_to_wick_ratio=1.5):
-        hmrs=[]
-        uphs,dwnhs=[],[]
-        for i,h0 in enumerate(hhs):
-            bod=cls[i]-ops[i]
-            l0,cl0,op0=lws[i],cls[i],ops[i]
-            try:
-                if bod<0: # down candle
-                    
-                    uph = abs(l0-cl0)/abs(h0-op0) # up/bull signal
-                    dwnh = abs(h0-op0)/abs(l0-cl0)
-                elif bod>0: # up candle
-                    uph = abs(l0-op0)/abs(h0-cl0)
-                    dwnh = abs(h0-cl0)/abs(l0-op0)
-                else:
-                    hmrs.append(0)
-                    continue
-                if uph > wick_to_wick_ratio :
-                    wick_size= (min(ops[i],cls[i])-l0) #lower :NOTE should be positive
-                    if abs(wick_size) / abs(bod) >  wick_to_body_ratio:
-                        expected_percent_change = (wick_size+abs(bod))/cl0
-                        hmrs.append(expected_percent_change)
-                    else:
-                        hmrs.append(0)
-                elif dwnh > wick_to_wick_ratio:
-                    wick_size= (max(ops[i],cls[i])-h0) # upper # NOTE should be negative
-                    if abs(wick_size) / abs(bod) >  wick_to_body_ratio:
-                        expected_percent_change = (wick_size-abs(bod))/cl0
-                        hmrs.append(expected_percent_change)
-                    else:
-                        hmrs.append(0)
-                else:
-                    hmrs.append(0)
-            except:
-                hmrs.append(0)
-        df['hammers'] = hmrs
-        #print(hmrs,'hammers')
-    def sentiment(df,df_d):
-        #https://medium.datadriveninvestor.com/sentiment-analysis-of-stocks-from-financial-news-using-python-82ebdcefb638?gi=a8849e546f46
-        df['sentiments'] = [0]*len(df['hammers'])
-        print('WARNING - Sentiments analysis not yet implemented')
-        pass
+   
 
-
-
-
-
-
-
-
-
-
-    
-    #NOTE every comment line below should have a corresponding function , if not assume this script is incomplete. For instance sub categories should have functions and parent categories should have functions that execute a custom grouping of its subcategory functions again , redundant but for compeltion. for instance a function to replace the price data retrieval method is not complete.
+    ##########################################
+    ##########################################
+    #### NOTES AND ROADMAP
+    ##########################################
+    ##########################################
+    # potential signals
     #get price data for symbol
     #get signals
     # -common Technical Analysis
-    rsi(df,rsi_len=7)
+    # -rsi
     # -extrema
     #extrema(df,n=5)
     # -candle patterns
     # -- engulfing
     #engulfing(df,cls,ops)
     # -- hammer
-    hammer(df,cls,ops,hhs,lws,wick_to_body_ratio=2,wick_to_wick_ratio=1.5)
     # -- stars
     # -- tweezers
     # -- rest
@@ -149,17 +203,12 @@ def main(sym='SPY',interval="1d",period='max',start=None,end=None,candle_range=N
     # --- flags
     # --- wedges
     # -sentiment
-    sentiment(df,df_d)
-    #get mtf bullish/bearish sequences
+    #get multi-timeframe bullish/bearish sequences
     # - connected, higher tf, specific signals
     
-    #print(df.columns)
-    print(f'\033[1;34;40m symbol: {sym} timeframe: {interval}\033[0;37;40m')
-    print('hammer signals:\n',df['hammers'][-10:])
-    print('current sentiment score (timeframe unknown... max data pulled):\n', df['sentiments'][-1:])
-    print(f'\033[1;33;40m {sym} timeframe {interval} current trade reccomendation:\n', df['hammers'][-1],'\n\033[0;37;40m')
-    # NOTE this function sends data up to a ranking function that takes inputs for how to weight each indicator signal and aggregates them into a tradeable prediction about price movement 
-    return df,df_d
+    # END FUNCTION #
+
+
 def bazinga_test():
     print(f'\033[1;34;40m Welcome to tf_analysis_bazinga_shortened.py \033[0;37;40m \n')
     print('\033[1;33;40m trade reccomendations are in yellow \033[0;37;40m')
